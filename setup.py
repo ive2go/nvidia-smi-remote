1b3335127b5f532d5726a9170c502ddcf1a3e5c1
heewon <ive2go@hdc-labs.com>
2025-04-03 10:43:34 +0900

    Update setup.py for new release version (0.1.1)
    - Bump version from 0.1.0 to 0.1.1 to address PyPI file reuse issue.
    - Update long_description configuration to load README.md for PyPI project description.
    - Prepare the project for the upcoming release and distribution on PyPI.

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="nvidia-smi-remote",
-    version="0.1.0",
-    description="CLI tool to query GPU status locally and remotely (based on nvidia-smi)",
+    version="0.1.1",
+    description="CLI tool to query GPU status remotely (based on nvidia-smi)",
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
     author="HEEWON KIM",  
     author_email="ive2go@naver.com", 
     packages=["nvidia_smi_remote"],
